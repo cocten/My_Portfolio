05120254fa276bd89167e3cd7f4bb1d49bb159b0
cocten <100824558+cocten@users.noreply.github.com>
2024-07-10 23:43:45 +0800

    Add files via upload

---
+++ C#/氣泡排序正解/氣泡排序正解/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace 氣泡排序正解
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 10, 4, 9, 1, 30, 5, 7, 25, 6, 3, 2, 8, 0 };
+            int i, j, t;
+
+            for (i = n.Length-1; i > 0; i--)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    if (n[j] > n[j + 1])
+                    {
+                        t = n[j];
+                        n[j] = n[j + 1];
+                        n[j + 1] = t;
+                    }
+                }              
+            }
+            for (i=0;i<n.Length;i++)
+            {
+                Console.Write(n[i] + "/n" +"/r");
+            }
+            /*
+            foreach (int number in n)
+            {
+                Console.WriteLine("{0} ", number);
+            }
+            */
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/練習1(總和與平均)/練習1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace 練習1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,a,total=0;
+
+            Console.Write("請輸入一個整數n:");
+            n = int.Parse(Console.ReadLine());
+            for (a = 1; a <=n; a=a-2)
+            {  
+                total = total + a;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("總和:"+total);
+            Console.WriteLine("平均:"+total / (double)n);
+            
+            Console.ReadLine();
+        }
+    }
+}
