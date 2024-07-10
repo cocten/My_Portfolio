3292f552b3b8c08a80323e1536d4bf3de02aa266
cocten <100824558+cocten@users.noreply.github.com>
2024-07-10 23:49:05 +0800

    Add files via upload

---
+++ C#/練習2(算瓶數)/練習2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 練習2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("請輸入瓶數:");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("有"+n/12+"打");
+            Console.WriteLine("剩"+n%12+"瓶");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/練習3(亂數總和)/練習3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace 練習3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,n,i,total=0;
+            Random r1 = new Random();
+
+            Console.Write("請輸入一個數n:");
+            n = int.Parse(Console.ReadLine());
+
+            for(i=1;i<=n;i++)
+            {
+              x = (r1.Next(1,26));
+              Console.WriteLine(x);
+                if (x%2.0 == 0)
+                {
+                    total = total + x;
+                }              
+            }
+
+            Console.WriteLine("總和:" + total);
+            Console.ReadLine();
+        }
+    }
+}
