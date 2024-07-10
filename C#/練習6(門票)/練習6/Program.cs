723b3eabe6ca04204b4f11801ae6efe214edda61
cocten <100824558+cocten@users.noreply.github.com>
2024-07-11 00:20:34 +0800

    Add files via upload

---
+++ C#/練習6(門票)/練習6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace 練習6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, x,p=0,p1=0;
+            double m;
+
+            Console.Write("請輸入旅遊團人數:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (i=1;i<=n;i++)
+            {
+                Console.Write("請輸入第" + i + "位年齡:");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (x <=6  || x >=65 )
+                {
+                    p =p+1 ;
+                }
+                else
+                {
+                    p1=p1+1;
+                }
+                
+            }
+
+            m = (p * 200 * (double)0.8) + p1*200;
+            Console.WriteLine();
+            Console.Write("旅遊團人數為"+ n +"人,其中"+ p +"人打八折,供需繳付"+ m +"元");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/練習8(倒直角三角形)/練習8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace 練習8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int H;
+            Console.Write("請輸入一整數 H：");
+            H = int.Parse(Console.ReadLine());
+            Console.WriteLine("輸出:");
+            if (H>=0 && H<=20)
+            {
+                for (int i = 1; i <= H; i++)
+                {
+                    for (int j = i; j <= H; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write("\n");
+                }
+               
+            }
+            if (H <= 0 || H > 20)
+            {
+                Console.Write("輸入錯誤");
+            }
+            Console.ReadLine();
+        }
+    }
+}
