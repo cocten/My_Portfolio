107a4f51c6e0398012e8db76e9f52ab19e505fe0
cocten <100824558+cocten@users.noreply.github.com>
2024-07-10 23:50:16 +0800

    Add files via upload

---
+++ C#/練習4(最大最小值)/練習4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace 練習4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,max = 0,min = 9999,i;
+
+            for (i = 1; i <= 5; i++)
+            {
+                Console.Write("請輸入整數:");
+                a = int.Parse(Console.ReadLine());
+
+                if (a >= max)
+                {
+                    max=a;
+                }
+                if (a <= min)
+                {
+                    min=a;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("最大值" + max);
+            Console.WriteLine("最小值" + min);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/練習5(費氏列數)/練習5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace 練習5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,N;
+            int  a = 1,b=1,c=(a+b);
+            Console.Write("請輸入一個整數N: ");
+            N = int.Parse(Console.ReadLine());
+
+           
+            Console.Write(a+" " +b);
+            
+            for (i =2; i < N; i++)
+            {
+                c = a + b;
+                Console.Write(" "+c);
+                a = b;
+                b = c;
+            }
+
+            Console.WriteLine();
+            Console.Write("列數總和:"+(a+b+c-1)+"  "+ "平均:" + ((a + b + c-1)/(double)N));
+            Console.ReadLine();
+        }
+    }
+}
